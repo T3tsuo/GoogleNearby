82bb4a4409d0a4a84c5a85bfcceaf69d5b30fcfb
T3tsuo <comeautakumi@gmail.com>
2023-03-24 21:43:01 -0300

    running program will send email everyone's distance to the logged in user

---
+++ GoogleNearby.py
@@ -0,0 +1,65 @@
+import datetime
+
+from locationsharinglib import Service
+import os
+import pickle
+from math import cos, asin, sqrt, pi
+import smtplib
+
+if os.path.isfile("email.dat"):
+    google_email = pickle.load(open("email.dat", "rb"))
+else:
+    google_email = input("Enter email linked to cookies. Do not use same account as the user being compared to: ")
+    pickle.dump(google_email, open("email.dat", "wb"))
+
+if os.path.isfile("mail_password.dat"):
+    mail_password = pickle.load(open("mail_password.dat", "rb"))
+else:
+    mail_password = input("Email's password (if using gmail, generate App Password): ")
+    pickle.dump(mail_password, open("mail_password.dat", "wb"))
+
+cookies_file = 'cookies.txt'
+
+service = Service(cookies_file=cookies_file, authenticating_account=google_email)
+
+
+def distance_km(lat1, lon1, lat2, lon2):
+    p = pi / 180
+    a = 0.5 - cos((lat2 - lat1) * p) / 2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    # radius of earth * 2 is that magic num
+    return 12742 * asin(sqrt(a))
+
+
+def ping_mail(user, password, mes):
+    sent_from = user
+    to = [user, user]
+    subject = "GoogleNearby"
+
+    email_text = """From: %s\nTo: %s\nSubject: %s\n\n%s
+        """ % (sent_from, ", ".join(to), subject, mes)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(user, password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
+
+
+def run():
+    message = ""
+    for person in service.get_all_people():
+        if person.full_name != google_email:
+            d = distance_km(service.get_coordinates_by_full_name(google_email)[0],
+                            service.get_coordinates_by_full_name(google_email)[1],
+                            person.latitude, person.longitude)
+            message += str(person.full_name) + " is " + str(round(d, 2)) + "km away\n"
+    ping_mail(google_email, mail_password, message)
+    print(message)
+
+
+run()
